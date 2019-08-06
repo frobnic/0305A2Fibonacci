5571b392007c68baa1e0acd12e098a5313714c19
frank paulsen <frobnic+ita@gmail.com>
2019-08-06 10:31:48 +0200

    code inserted

--- 0305A2Fibonacci/Program.cs
+++ 0305A2Fibonacci/Program.cs
@@ -10,6 +10,26 @@ namespace _0305A2Fibonacci
     {
         static void Main(string[] args)
         {
+            int n = 2; // Schleifenvariable, fib(0) und fib(1) werden vorher ausgegeben.
+            int a = 0; // zweiter vorgaenger
+            int b = 1; // erster vorgaenger
+            int f = 0; // Fibonacci-zahl
+
+            Console.WriteLine("fib({0,2}) = {1,8}  ", 1, 0);
+            Console.WriteLine("fib({0,2}) = {1,8}  ", 2, 1);
+            while (n < 30)
+            {
+
+                n++;
+
+                f = a + b;
+                a = b;
+                b = f;
+                Console.WriteLine("fib({0,2}) = {1,8}  ", n, f);
+
+
+
+            }
         }
     }
 }
